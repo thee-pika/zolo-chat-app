6969889dfb7594557656bb7b69b5a182ab81ac02
Mogili Deepika <mogilideepika218@gmail.com>
2025-05-17 20:43:33 +0530

    started with backend

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {config} from "dotenv";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req , res) => {
+    res.send({ message: "App is saying Hii"});
+})
+
+app.listen(PORT, () => { console.log("App is running on", PORT)});
